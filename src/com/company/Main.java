dc8b94c180cde9c15536112f6ba7ce4411b31b8d
SamBam451 <smbovshovets@gmail.com>
2022-01-20 14:26:09 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Samuel Bovshovets
+        System.out.println("Hello world");
+        String city = "Galloway";
+        int zip = 43119;
+        int high[] = {32,25,27,40,45};
+        double av = (high[0]+high[1]+high[2]+high[3]+high[4])/5.0;
+        System.out.println("City: " + city + " Zip Code: " + zip + " Average High Temperature: " + av);
+    }
+}
